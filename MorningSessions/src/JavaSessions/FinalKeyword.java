ec6e0f5fc7869b92fb409d22b560c9a1e4b1ee85
e3009192 <e3009192@BLR1-LP34YG622.FNFIS.com>
2018-04-09 12:05:19 +0530

    updated FinalKeyword class

--- MorningSessions/src/JavaSessions/FinalKeyword.java
+++ MorningSessions/src/JavaSessions/FinalKeyword.java
@@ -4,13 +4,14 @@ public class FinalKeyword {
 
 	public static void main(String[] args) {
 
-		final int wheels = 4;
-		
+		int wheels = 4;
+		wheels= wheels + 5;
 		final int i = 10;
 //		i = 20;
 //		i = 30;
 		
 		System.out.println(i);
+		System.out.println(wheels);
 		
 		
 	}
